fe2897a538546aac67cae260769a07093c0bb4f9
JaviMx <javimxmine@hotmail.com>
2022-05-04 18:12:20 -0500

    Pruebas StudentService y correccion

---
+++ test/lib/services/studentsService.test.js
@@ -0,0 +1,51 @@
+const students = [
+    {
+        "id": "6264d5d8b4b46a11ea710c21",
+        "name": "Obrien",
+        "email": "Gracie@visualpartnership.xyz",
+        "credits": 876,
+        "enrollments": [
+          "Visual Thinking Avanzado",
+          "Visual Thinking Intermedio"
+        ],
+        "previousCourses": 3,
+        "haveCertification": true
+      },
+      {
+        "id": "6264d5d8d6646dc0187f08d3",
+        "name": "Orr",
+        "email": "Nikki@visualpartnership.xyz",
+        "credits": 225,
+        "enrollments": [
+          "Visual Thinking Básico",
+          "Visual Thinking Avanzado"
+        ],
+        "previousCourses": 10,
+        "haveCertification": false
+      }    
+]
+
+const StudentsService = require("../../../lib/services/StudentsService")
+
+
+
+describe("Puebas Students Services", () => {
+
+    test("Metodo allStudents", () => { 
+        const allStudents = StudentsService.allStudents(students);
+        expect(allStudents.length).toBe(students.length);
+    });
+     
+    test("Metodo emailStudentHaveCertification", () => { 
+        const emailStudentHaveCertification = StudentsService.emailStudentHaveCertification(students);
+        expect(emailStudentHaveCertification.length).toBe(1);
+    });
+
+    test("Metodo studentsCredits", () => { 
+        const studentsCredits = StudentsService.studentsCredits(students, 500);
+        expect(studentsCredits.length).toBe(1);
+    });
+
+      
+
+});
