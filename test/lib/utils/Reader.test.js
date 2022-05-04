28f1899b03722ce5d6354fdd4149c4e51c6d9a71
JaviMx <javimxmine@hotmail.com>
2022-05-04 14:49:32 -0500

    Prueba a Reader agregada

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,10 @@
+const Reader = require( "../../../lib/utils/Reader.js" )
+const datos = require("../../../visualpartners.json")
+
+describe("Puebas a Reader", () => {
+
+    test('Leer el archivo Json', () => { 
+        const visualpartners = Reader.readJsonFile()
+        expect(visualpartners.length).toBe(datos.length)
+     })
+}); 
